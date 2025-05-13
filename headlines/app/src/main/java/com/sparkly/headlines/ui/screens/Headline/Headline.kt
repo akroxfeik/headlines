e1ed34edf031385e90842f85012f064e43bf61d8
fernando-carvalho-91 <114769018+fernando-carvalho-91@users.noreply.github.com>
2025-05-14 00:26:52 +0100

    screen headline details init

---
+++ headlines/app/src/main/java/com/sparkly/headlines/ui/screens/Headline/Headline.kt
@@ -0,0 +1,9 @@
+package com.sparkly.headlines.ui.screens.Headline
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+fun Headline(viewModel: HeadlineViewModel = hiltViewModel()) {
+
+}
