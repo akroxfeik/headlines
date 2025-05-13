426c7d0ba971db15764595639e852386879b3597
fernando-carvalho-91 <114769018+fernando-carvalho-91@users.noreply.github.com>
2025-05-13 01:43:09 +0100

    setup hilt entry point

--- headlines/app/src/main/java/com/sparkly/headlines/MainActivity.kt
+++ headlines/app/src/main/java/com/sparkly/headlines/MainActivity.kt
@@ -12,7 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.sparkly.headlines.ui.theme.HeadlinesTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
