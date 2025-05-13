a436a840e455d48428dbddf1011612af67cf8ad9
fernando-carvalho-91 <114769018+fernando-carvalho-91@users.noreply.github.com>
2025-05-13 01:49:42 +0100

    setup data class for our obj headline

--- headlines/app/src/main/java/com/sparkly/headlines/data/model/Headline.kt
+++ headlines/app/src/main/java/com/sparkly/headlines/data/model/Headline.kt
@@ -3,6 +3,36 @@ package com.sparkly.headlines.data.model
 import com.squareup.moshi.Json
 
 data class Headline(
+    @Json(name = "status")
+    val status: String,
+    @Json(name = "totalResults")
+    val totalResults: Int,
+    @Json(name = "articles")
+    val articles: List<Article>
+)
+
+data class Article(
+    @Json(name = "source")
+    val source: Source,
+    @Json(name = "author")
+    val author: String,
+    @Json(name = "title")
+    val title: String,
+    @Json(name = "description")
+    val description: String,
+    @Json(name = "url")
+    val url: String,
+    @Json(name = "urlToImage")
+    val urlToImage: String,
+    @Json(name = "publishedAt")
+    val publishedAt: String,
+    @Json(name = "content")
+    val content: String
+)
+
+data class Source(
     @Json(name = "id")
-    val id: String
+    val id: String,
+    @Json(name = "name")
+    val name: String
 )
